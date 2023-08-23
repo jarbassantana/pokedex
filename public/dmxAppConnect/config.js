72859813055330f7cf754c0293c440bb13c6e43d
Pro Bro <kopitik667@busantei.com>
2023-08-23 01:26:25 -0300

    init commit

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,409 @@
+dmx.config({
+  "index": {
+    "api1": [
+      {
+        "type": "object",
+        "name": "data",
+        "sub": [
+          {
+            "type": "number",
+            "name": "count"
+          },
+          {
+            "type": "text",
+            "name": "next"
+          },
+          {
+            "type": "text",
+            "name": "previous"
+          },
+          {
+            "type": "array",
+            "name": "results",
+            "sub": [
+              {
+                "type": "text",
+                "name": "name"
+              },
+              {
+                "type": "text",
+                "name": "url"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "type": "object",
+        "name": "headers",
+        "sub": [
+          {
+            "type": "text",
+            "name": "access-control-allow-origin"
+          },
+          {
+            "type": "text",
+            "name": "alt-svc"
+          },
+          {
+            "type": "text",
+            "name": "cache-control"
+          },
+          {
+            "type": "text",
+            "name": "cf-cache-status"
+          },
+          {
+            "type": "text",
+            "name": "cf-ray"
+          },
+          {
+            "type": "text",
+            "name": "content-encoding"
+          },
+          {
+            "type": "text",
+            "name": "content-type"
+          },
+          {
+            "type": "text",
+            "name": "date"
+          },
+          {
+            "type": "text",
+            "name": "etag"
+          },
+          {
+            "type": "text",
+            "name": "function-execution-id"
+          },
+          {
+            "type": "text",
+            "name": "nel"
+          },
+          {
+            "type": "text",
+            "name": "report-to"
+          },
+          {
+            "type": "text",
+            "name": "server"
+          },
+          {
+            "type": "text",
+            "name": "strict-transport-security"
+          },
+          {
+            "type": "text",
+            "name": "vary"
+          },
+          {
+            "type": "text",
+            "name": "x-cache"
+          },
+          {
+            "type": "text",
+            "name": "x-cache-hits"
+          },
+          {
+            "type": "text",
+            "name": "x-cloud-trace-context"
+          },
+          {
+            "type": "text",
+            "name": "x-country-code"
+          },
+          {
+            "type": "text",
+            "name": "x-orig-accept-language"
+          },
+          {
+            "type": "text",
+            "name": "x-powered-by"
+          },
+          {
+            "type": "text",
+            "name": "x-served-by"
+          },
+          {
+            "type": "text",
+            "name": "x-timer"
+          }
+        ]
+      }
+    ],
+    "pokeapi": [
+      {
+        "type": "object",
+        "name": "data",
+        "sub": [
+          {
+            "type": "number",
+            "name": "count"
+          },
+          {
+            "type": "text",
+            "name": "next"
+          },
+          {
+            "type": "text",
+            "name": "previous"
+          },
+          {
+            "type": "array",
+            "name": "results",
+            "sub": [
+              {
+                "type": "text",
+                "name": "name"
+              },
+              {
+                "type": "text",
+                "name": "url"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "type": "object",
+        "name": "headers",
+        "sub": [
+          {
+            "type": "text",
+            "name": "access-control-allow-origin"
+          },
+          {
+            "type": "text",
+            "name": "alt-svc"
+          },
+          {
+            "type": "text",
+            "name": "cache-control"
+          },
+          {
+            "type": "text",
+            "name": "cf-cache-status"
+          },
+          {
+            "type": "text",
+            "name": "cf-ray"
+          },
+          {
+            "type": "text",
+            "name": "content-encoding"
+          },
+          {
+            "type": "text",
+            "name": "content-type"
+          },
+          {
+            "type": "text",
+            "name": "date"
+          },
+          {
+            "type": "text",
+            "name": "etag"
+          },
+          {
+            "type": "text",
+            "name": "function-execution-id"
+          },
+          {
+            "type": "text",
+            "name": "nel"
+          },
+          {
+            "type": "text",
+            "name": "report-to"
+          },
+          {
+            "type": "text",
+            "name": "server"
+          },
+          {
+            "type": "text",
+            "name": "vary"
+          },
+          {
+            "type": "text",
+            "name": "x-cache"
+          },
+          {
+            "type": "text",
+            "name": "x-cache-hits"
+          },
+          {
+            "type": "text",
+            "name": "x-cloud-trace-context"
+          },
+          {
+            "type": "text",
+            "name": "x-country-code"
+          },
+          {
+            "type": "text",
+            "name": "x-orig-accept-language"
+          },
+          {
+            "type": "text",
+            "name": "x-powered-by"
+          },
+          {
+            "type": "text",
+            "name": "x-served-by"
+          },
+          {
+            "type": "text",
+            "name": "x-timer"
+          }
+        ]
+      }
+    ],
+    "pokeapidetails": [
+      {
+        "type": "object",
+        "name": "data",
+        "sub": [
+          {
+            "type": "number",
+            "name": "count"
+          },
+          {
+            "type": "text",
+            "name": "next"
+          },
+          {
+            "type": "text",
+            "name": "previous"
+          },
+          {
+            "type": "array",
+            "name": "results",
+            "sub": [
+              {
+                "type": "text",
+                "name": "name"
+              },
+              {
+                "type": "text",
+                "name": "url"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "type": "object",
+        "name": "headers",
+        "sub": [
+          {
+            "type": "text",
+            "name": "access-control-allow-origin"
+          },
+          {
+            "type": "text",
+            "name": "alt-svc"
+          },
+          {
+            "type": "text",
+            "name": "cache-control"
+          },
+          {
+            "type": "text",
+            "name": "cf-cache-status"
+          },
+          {
+            "type": "text",
+            "name": "cf-ray"
+          },
+          {
+            "type": "text",
+            "name": "content-encoding"
+          },
+          {
+            "type": "text",
+            "name": "content-type"
+          },
+          {
+            "type": "text",
+            "name": "date"
+          },
+          {
+            "type": "text",
+            "name": "etag"
+          },
+          {
+            "type": "text",
+            "name": "function-execution-id"
+          },
+          {
+            "type": "text",
+            "name": "nel"
+          },
+          {
+            "type": "text",
+            "name": "report-to"
+          },
+          {
+            "type": "text",
+            "name": "server"
+          },
+          {
+            "type": "text",
+            "name": "vary"
+          },
+          {
+            "type": "text",
+            "name": "x-cache"
+          },
+          {
+            "type": "text",
+            "name": "x-cache-hits"
+          },
+          {
+            "type": "text",
+            "name": "x-cloud-trace-context"
+          },
+          {
+            "type": "text",
+            "name": "x-country-code"
+          },
+          {
+            "type": "text",
+            "name": "x-orig-accept-language"
+          },
+          {
+            "type": "text",
+            "name": "x-powered-by"
+          },
+          {
+            "type": "text",
+            "name": "x-served-by"
+          },
+          {
+            "type": "text",
+            "name": "x-timer"
+          }
+        ]
+      }
+    ],
+    "serverconnect1": {
+      "meta": null,
+      "outputType": "text"
+    },
+    "repeat2": {
+      "meta": [
+        {
+          "type": "text",
+          "name": "name"
+        },
+        {
+          "type": "text",
+          "name": "url"
+        }
+      ],
+      "outputType": "array"
+    }
+  }
+});
